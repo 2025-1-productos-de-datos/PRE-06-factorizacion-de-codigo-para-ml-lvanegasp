2004ee3345848a571b9f9fc1a75f512f6a8c346b
Lucas Vanegas Palacio <lvanegasp@unal.edu.co>
2025-05-24 08:12:36 -0500

    primer commit

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+"""Entry point for the homework package."""
+
+# python3 -m homework --model elasticnet --l1_ratio 0.1 --alpha 0.1
+
+from homework.src.main import main
+
+if __name__ == "__main__":
+    main()
